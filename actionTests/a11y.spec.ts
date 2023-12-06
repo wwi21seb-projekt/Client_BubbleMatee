15d3ff02d558f05e8cfd541942df27475d5afd0d
johannadke <johanna.dke@web.de>
2023-12-06 17:54:59 +0100

    ci first code checks

--- actionTests/a11y.spec.ts
+++ actionTests/a11y.spec.ts
@@ -1,17 +1,15 @@
-const pa11y = require("pa11y");
+const pa11y = require('pa11y');
 
- (async function () {
-        try {
-            const result = await pa11y('http://localhost:5173', {
-                   includeWarnings: true,
-                   level: "error",
-                   runners: [
-                       "htmlcs"
-                   ]
-              })
+(async function () {
+	try {
+		const result = await pa11y('http://localhost:5173', {
+			includeWarnings: true,
+			level: 'error',
+			runners: ['htmlcs']
+		});
 
-             console.log(result)
-           } catch (e) {
-               throw new Error(e)
-           }
-   } )();
+		console.log(result);
+	} catch (e) {
+		throw new Error(e);
+	}
+})();
