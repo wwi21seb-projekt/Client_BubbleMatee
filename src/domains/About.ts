2695dcf675074b460835f2846f421ec7710c9513
johannadke <johanna.dke@web.de>
2023-12-11 15:47:07 +0100

    feat: setup folder structure

---
+++ src/domains/About.ts
@@ -0,0 +1,20 @@
+// Definieren einer Schnittstelle für Personendaten
+export interface PersonData {
+	position: number;
+	description: string;
+	value: string;
+}
+
+// Definieren einer Schnittstelle für die Quelldaten der Personen
+export interface PersonSourceData {
+	id: string;
+	src: string;
+	data: PersonData[];
+}
+
+export interface Logo {
+	name: string;
+	url: string;
+	src: string;
+	description: string;
+}
---
+++ src/domains/index.ts
@@ -0,0 +1,1 @@
+export type { Logo, PersonData, PersonSourceData } from './About';
