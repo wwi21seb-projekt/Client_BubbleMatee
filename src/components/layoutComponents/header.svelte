5318d3b8b79c57ddf2f64ee2baad39facf0c7fbe
johannadke <johanna.dke@web.de>
2023-12-23 21:11:26 +0100

    feat: optimize text size for galaxy fold

--- src/components/layoutComponents/header.svelte
+++ src/components/layoutComponents/header.svelte
@@ -1,17 +1,25 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import { Settings } from '$components';
+	import ArrowLeftIcon from '$images/icons/arrow-left-icon.svelte';
 	import { AppBar, LightSwitch, modeCurrent, setModeUserPrefers } from '@skeletonlabs/skeleton';
 
 	const handleLightSwitch = () => {
 		setModeUserPrefers($modeCurrent);
 	};
+
+	/* const handleBack = async () => {
+		history.back();
+	}; */
 </script>
 
 <!-- App Shell -->
 <AppBar padding="p-1">
 	<svelte:fragment slot="lead">
-		<div class="flex">
+		<div class="flex items-center">
+			<!-- <button on:click={handleBack}>
+				<ArrowLeftIcon />
+			</button> -->
 			<a href="/">
 				<img
 					class="h-10 md:h-16 self-center"
--- src/components/profile/profile-header.svelte
+++ src/components/profile/profile-header.svelte
@@ -101,21 +101,21 @@
 	};
 </script>
 
-<div class="p-4 grid grid-cols-4 gap-1 font-semibold">
+<div class="p-4 grid grid-cols-4 gap-0 font-semibold">
 	<div class="flex flex-col self-center justify-center">
 		<Avatar initials={name.charAt(0)} background="bg-surface-300" width="w-22 lg:w-18 xl:w-16  " />
 	</div>
 	<div class="flex flex-col self-center">
 		<div class="place-self-center">{user.posts}</div>
-		<div class="place-self-center">Posts</div>
+		<div class="place-self-center text-[11px] sm:text-base">Posts</div>
 	</div>
 	<div class="flex flex-col self-center">
 		<div class="place-self-center">{user.follower}</div>
-		<div class="place-self-center">Abonnenten</div>
+		<div class="place-self-center text-[11px] sm:text-base">Abonnenten</div>
 	</div>
 	<div class="flex flex-col self-center">
 		<div class="place-self-center">{user.following}</div>
-		<div class="place-self-center">Abonnierte</div>
+		<div class="place-self-center text-[11px] sm:text-base">Abonnierte</div>
 	</div>
 </div>
 <div class="px-4 pb-1 font-semibold">{name}</div>
