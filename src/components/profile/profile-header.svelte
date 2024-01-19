f1983770a5f5d524e9c2bb2c44db1838295b41bf
pbahde <bahde.paul@gmail.com>
2024-01-19 13:12:29 +0100

    fix

--- src/components/profile/profile-header.svelte
+++ src/components/profile/profile-header.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import type { UserInfo } from '$domains';
-	import { Avatar, getModalStore, getToastStore, type ModalComponent, type ModalSettings, type ToastSettings } from '@skeletonlabs/skeleton';
+	import {
+		Avatar,
+		getModalStore,
+		getToastStore,
+		type ModalComponent,
+		type ModalSettings,
+		type ToastSettings
+	} from '@skeletonlabs/skeleton';
 	import { currentUsername } from '$stores';
 	import { page } from '$app/stores';
 	import { invalidateAll } from '$app/navigation';
@@ -102,30 +109,35 @@
 	};
 
 	function handlePictureClick(): void {
-		user.profilePictureUrl
-			const modalComponent: ModalComponent = {
-				ref: Avatar,
-				
-				props: { src: (user.profilePictureUrl ? user.profilePictureUrl : '/src/images/icons/person.png'),
-				width: "w-1/2 sm:w-1/3 md:w-1/6" }
-			};
-			const modal: ModalSettings = {
-				type: 'component',
-				component: modalComponent,
-				backdropClasses:
-					'bg-transparent'
-			};
-			modalStore.trigger(modal);
+		user.profilePictureUrl;
+		const modalComponent: ModalComponent = {
+			ref: Avatar,
+
+			props: {
+				src: user.profilePictureUrl ? user.profilePictureUrl : '/src/images/icons/person.png',
+				width: 'w-1/2 sm:w-1/3 md:w-1/6'
+			}
+		};
+		const modal: ModalSettings = {
+			type: 'component',
+			component: modalComponent,
+			backdropClasses: 'bg-transparent'
+		};
+		modalStore.trigger(modal);
 	}
 </script>
 
 <div class="flex w-full justify-center items-center">
 	<div class="w-full px-4 pt-2 sm:w-3/4 md:w-full lg:w-3/4 align-self">
-		<div class="w-full ">
+		<div class="w-full">
 			<div class="grid grid-cols-4 gap-0 font-semibold p-4">
 				<div class="flex flex-col self-center justify-center w-full">
-					<Avatar border="hover:border-2 hover:!border-surface-600"
-							cursor="cursor-pointer" src={user.profilePictureUrl ? user.profilePictureUrl : '/src/images/icons/person.png'} on:click={handlePictureClick}/>
+					<Avatar
+						border="hover:border-2 hover:!border-surface-600"
+						cursor="cursor-pointer"
+						src={user.profilePictureUrl ? user.profilePictureUrl : '/src/images/icons/person.png'}
+						on:click={handlePictureClick}
+					/>
 				</div>
 				<div class="flex flex-col self-center">
 					<div class="place-self-center">{user.posts}</div>
@@ -140,14 +152,16 @@
 					<div class="place-self-center text-[11px] sm:text-base">Abonnierte</div>
 				</div>
 			</div>
+			<!--Show the Follow-Button next to the username, if the user has no status-->
 			<div class="flex flex-col md:flex-row justify-center md:justify-between items-center px-4">
 				<div class="flex flex-row items-center w-full">
-				<h3 class="pr-2 font-bold text-xl md:text-2xl">{name}</h3>
-				{#if user.nickname}
-				<span class="inline-block h-auto w-px self-stretch bg-surface-600" />
-				<small class="pl-2 text-lg md:text-xl">{user.nickname}</small>
-				{/if}
+					<h3 class="pr-2 font-bold text-xl md:text-2xl">{name}</h3>
+					{#if user.nickname}
+						<span class="inline-block h-auto w-px self-stretch bg-surface-600" />
+						<small class="pl-2 text-lg md:text-xl">{user.nickname}</small>
+					{/if}
 				</div>
+				<!--Show the Follow-Button next to the username, if the user has no status-->
 				{#if !isOwnUser && !user.status}
 					<button class={setFollowButtonClass()} on:click={handleButtonClick}
 						>{setFollowButtonText()}</button
@@ -155,10 +169,13 @@
 				{/if}
 			</div>
 
-			<div class="w-full px-4 flex flex-col md:flex-row justify-center md:justify-between items-center">
+			<div
+				class="w-full px-4 flex flex-col md:flex-row justify-center md:justify-between items-center"
+			>
 				<small class="text-base md:text-lg break-words w-full">
 					{user.status}
-				</small>			
+				</small>
+				<!--Show the Follow-Button next to the status, if the user has a status-->
 				{#if !isOwnUser && user.status}
 					<button class={setFollowButtonClass()} on:click={handleButtonClick}
 						>{setFollowButtonText()}</button
