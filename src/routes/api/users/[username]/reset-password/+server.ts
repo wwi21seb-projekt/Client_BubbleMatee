3e5db68d35bc9e2340186619e792d62292a47850
pbahde <bahde.paul@gmail.com>
2024-06-04 16:41:07 +0200

    fix

--- src/routes/api/users/[username]/reset-password/+server.ts
+++ src/routes/api/users/[username]/reset-password/+server.ts
@@ -1,4 +1,4 @@
-import type { ErrorResponse, FeedResponse } from '$domains';
+import type { ErrorResponse } from '$domains';
 import { PUBLIC_BASE_URL } from '$env/static/public';
 import { json, type RequestHandler } from '@sveltejs/kit';
 
