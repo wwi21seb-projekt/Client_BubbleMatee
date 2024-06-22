2d27a9c260f7fdad3e4f5723d766c2f68d53751d
zimmerai <aidanmichael@web.de>
2024-06-22 13:22:07 +0200

    fix: chat header behaviour on mobile

--- src/components/chats/chat-mobile-header.svelte
+++ src/components/chats/chat-mobile-header.svelte
@@ -9,11 +9,8 @@
 	export let chatPartner: Author;
 </script>
 
-<button
-	class="flex items-center bg-surface-50 dark:bg-surface-900 hover:bg-surface-100 dark:hover:bg-surface-800 w-full border-b border-surface-500/30"
-	on:click={() => {
-		goto(`/home/user/${chatPartner.username}`);
-	}}
+<div
+	class="flex items-center bg-surface-50 dark:bg-surface-900 w-full border-b border-surface-500/30"
 >
 	<button
 		class="lg:hidden h-12 btn"
@@ -23,10 +20,17 @@
 	>
 		<Icon src={ChevronLeft} />
 	</button>
-	<Avatar
-		class="m-2"
-		src={chatPartner.picture && chatPartner.picture.url ? chatPartner.picture.url : Person}
-		height="auto"
-	/>
-	<h3 class="flex-1 text-start font-bold ml-2 text-xl md:text-2xl">{chatPartner.username}</h3>
-</button>
+	<button
+		class="flex items-center hover:bg-surface-100 dark:hover:bg-surface-800"
+		on:click={() => {
+			goto(`/home/user/${chatPartner.username}`);
+		}}
+	>
+		<Avatar
+			class="m-2"
+			src={chatPartner.picture && chatPartner.picture.url ? chatPartner.picture.url : Person}
+			height="auto"
+		/>
+		<h3 class="flex-1 text-start font-bold ml-2 text-xl md:text-2xl">{chatPartner.username}</h3>
+	</button>
+</div>
