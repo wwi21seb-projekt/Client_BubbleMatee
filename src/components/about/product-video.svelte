4fccadf04e80d63137e970a8a092c0cc3eea797e
BraunJonas <95094614+BraunJonas@users.noreply.github.com>
2024-07-25 13:17:25 +0200

    Changing iframe and documentationtext for impressum

--- src/components/about/product-video.svelte
+++ src/components/about/product-video.svelte
@@ -23,7 +23,7 @@
 		<iframe
 			width="560"
 			height="315"
-			src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=oXg6NlZ94uWhuo1h"
+			src="https://www.youtube.com/embed/5iVtGNfrVyQ"
 			title="YouTube video player"
 			frameborder="0"
 			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
